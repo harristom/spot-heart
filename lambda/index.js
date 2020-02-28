4dee5f5f15e97175b93f5ab9ec521565c25a7760
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2020-02-28 00:16:36 +0000

    updated lambda/index.js.

--- lambda/index.js
+++ lambda/index.js
@@ -35,9 +35,9 @@ const LikeThisIntentHandler = {
               'Content-Type': 'application/json',
               Authorization: `Bearer ${accessToken}`,
             };
-            var { data } = await axios.get('https://api.spotify.com/v1/me/player/currently-playing', { headers });
-            console.log(data.body);
-            const speakOutput = `You're listening to ${data.item.name}`;
+            const response = await axios.get('https://api.spotify.com/v1/me/player/currently-playing', { headers });
+            console.log(response.data);
+            const speakOutput = `You're listening to ${response.data.item.name}`;
             return handlerInput.responseBuilder
                 .speak(speakOutput)
                 .getResponse();
